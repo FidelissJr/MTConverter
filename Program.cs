fac21b48ddb04e38b8188d0941291dc2346c221a
André Fidelis <andre.junior@sensustec.com.br>
2024-05-23 10:02:46 -0300

    Adicionado somente logica p implementar simulação mtdi em mt

--- Program.cs
+++ Program.cs
@@ -2,10 +2,12 @@
 
 //Console.WriteLine("Hello, World!");
 
+using System.Reflection;
+
 class MTConverter
 {
-    private static readonly string directory = "C:\\Fontes\\MT_Converter\\MT_Converter\\";
-    private static readonly string inputFile = directory + "\\entrada.txt";
+    private static readonly string directory = GetCurrentDirectory();
+    private static readonly string inputFile = $"{directory}\\entrada.txt";
     private static readonly string outFile = $"{directory}\\saida.txt";
 
     private static readonly string EstadoAuxiliar = "aux0";
@@ -46,6 +48,7 @@ class MTConverter
                 continue;
             }
 
+            //Caso o comentário esteja após a configuração, na mesma linha, a validação ocorre somente na configuração
             string[] parts = line.Contains(';') ? line.Split(';')[0].Trim().Split() : line.Trim().Split();
 
             if (parts.Length == 5)
@@ -122,4 +125,76 @@ class MTConverter
             writer.WriteLine(item);
     }
 
+    public static string GetCurrentDirectory()
+    {
+        // Obtém o caminho completo do arquivo em execução
+        string executablePath = Assembly.GetExecutingAssembly().Location.Split("bin").First();     
+
+        // Retorna o diretório do arquivo
+        return Path.GetDirectoryName(executablePath);
+    }
+
+    static private void SimulaMTDIEmMT()
+    {
+        using StreamReader reader = new(inputFile);
+        using StreamWriter writer = new(outFile);
+        string line;
+
+        List<string> novasConfiguracoes = new()
+        {
+            "0 0 0 l 0",
+            "0 1 1 l 0",
+            $"0 * # r {EstadoAuxiliar}"
+        };
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            if (IgnorarLinha(line))
+            {
+                //Caso a linha seja um comentario, somente escrever ela na saida
+                writer.WriteLine(line);
+                continue;
+            }
+
+            //Caso o comentário esteja após a configuração, na mesma linha, a validação ocorre somente na configuração
+            string[] parts = line.Contains(';') ? line.Split(';')[0].Trim().Split() : line.Trim().Split();
+
+            if (parts.Length == 5)
+            {
+                string currentState = parts[0];
+                string currentSymbol = parts[1];
+                string newSymbol = parts[2];
+                string direction = parts[3];
+                string newState = parts[4];
+
+                if (newState == EstadoInicial || currentState == EstadoInicial)
+                    line = SubstituirEstado(line, EstadoInicial, EstadoAuxiliar);
+
+                if (direction == MovimentoEsquerda && (newState != EstadoRejeicao && newState != EstadoAceitacao))
+                {
+                    //Faz a tratativa deste movimento p esquerda
+                    currentState = newState;
+                    currentSymbol = delimitador;
+                    newSymbol = delimitador;
+                    direction = MovimentoDireita;
+
+                    string novaConfiguracao = $"{currentState} {currentSymbol} {newSymbol} {direction} {newState}";
+
+                    //Verifica se a lista de novas configurações ja possui a configuração
+                    if (!novasConfiguracoes.Contains(novaConfiguracao))
+                        novasConfiguracoes.Add(novaConfiguracao);
+                }
+
+                writer.WriteLine(line);
+
+            }
+            else
+            {
+                Console.WriteLine("Erro na formatação da função sigma. Linha: " + line);
+                break;
+            }
+        }
+
+        AdicionarNovasConfiguracoes(writer, novasConfiguracoes);
+    }
 }
